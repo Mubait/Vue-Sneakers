4bcdfd3699f7856495781611838b00907bdd1430
mubait <kuz2001min@mail.ru>
2024-09-03 19:01:38 +0700

    Разработаны макеты основной страницы и корзины сайта

---
+++ src/components/CardList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from './Card.vue'
+
+const add = () => alert("added!");
+</script>
+
+<template>
+    <div class="grid grid-cols-4 gap-5">
+        <Card 
+        title="Мужские Кроссовки Nike Blazer Mid Suede" 
+        image-url="/sneakers/sneakers-1.jpg" 
+        :price="5000"
+        :is-added="false"
+        :is-favorite="false"
+        :on-click-add="add"
+        />
+    </div>
+</template>
