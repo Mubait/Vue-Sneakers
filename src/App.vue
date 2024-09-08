e7120a606c70ae5a848a6d208197485f38a7967e
mubait <kuz2001min@mail.ru>
2024-09-08 22:18:42 +0700

    Разработаны ф-и сортировки, поиска, добавления в 'любимые' кроссовок

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,86 @@
 <script setup>
+  import axios from 'axios'
+  import { ref, reactive, onMounted, watch, provide } from 'vue'
+
   import Header from './components/Header.vue'
   import CardList from './components/CardList.vue'
   import Drawer from './components/Drawer.vue'
+
+
+  const items = ref([])
+
+  const filters = reactive({
+    sortBy: 'title',
+    searchQuery: ''
+  })
+
+  const onChangeSelect = event => {
+    filters.sortBy = event.target.value;
+  }
+
+  const onChangeSearchInput = event => {
+    filters.searchQuery = event.target.value;
+  }
+
+  const fetchFavorites = async () => {
+    try {
+      const { data: favorites } = await axios.get('https://5b70284b34591f86.mokky.dev/favorites')
+      
+      items.value = items.value.map(item => {
+        const favorite = favorites.find(favorite => favorite.parentId === item.id);
+
+        if(!favorite) {
+          return item;
+        }
+
+        return {
+          ...item,
+          isFavorite: true,
+          favoriteId: favorite.id,
+        }
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const addToFavorite = async(item) => {
+    item.isFavorite = !item.isFavorite;
+
+    console.log(item);
+  }
+
+
+
+  const fetchItems = async() => {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`;
+    }
+    try {
+      const { data } = await axios.get('https://5b70284b34591f86.mokky.dev/items', {params})
+      
+      items.value = data.map(obj => ({
+        ...obj,
+        isFavorite: false,
+        isAdded: false
+      }))
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  onMounted(async () => {
+    fetchItems();
+    fetchFavorites();
+  })
+
+  watch(filters, fetchItems)
+
+  provide('addToFavorite', addToFavorite);
 </script>
 
 <template>
@@ -16,21 +95,23 @@
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
         
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none">
-            <option>По названию</option>
-            <option>По цене (дешёвые)</option>
-            <option>По цене (дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешёвые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" />
-            <input class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400" placeholder="Поиск.." />
+            <input @input="onChangeSearchInput" class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400" placeholder="Поиск.." />
           </div>
         </div>
       </div>
 
 
-      <CardList />
+      <div class="mt-10">
+        <CardList :items="items"/>
+      </div>
     </div>
   </div>
 
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,18 +1,32 @@
 <script setup>
+import { inject } from 'vue';
+
 import Card from './Card.vue'
 
-const add = () => alert("added!");
+defineProps({
+    items: Array
+})
+
+const onClickAdd = () => alert("added!");
+
+const addToFavorite = inject('addToFavorite');
 </script>
 
 <template>
     <div class="grid grid-cols-4 gap-5">
         <Card 
-        title="Мужские Кроссовки Nike Blazer Mid Suede" 
-        image-url="/sneakers/sneakers-1.jpg" 
-        :price="5000"
+        v-for="item in items.slice(0, 12)"
+        :key="item.id"
+        :id = "item.id"
+        :title="item.title" 
+        :image-url="item.imageUrl" 
+        :price="item.price"
         :is-added="false"
-        :is-favorite="false"
-        :on-click-add="add"
+        :is-favorite="item.isFavorite"
+        :on-click-add="onClickAdd"
+        :on-click-favorite="() => addToFavorite(item)"
         />
     </div>
+
+
 </template>
